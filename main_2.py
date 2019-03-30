715c5c7c7398627de31e955090379546d4935687
abhishektaur123 <abhishektaur123@gmail.com>
2019-03-30 16:36:17 -0500

    updated main

--- main_2.py
+++ main_2.py
@@ -35,6 +35,21 @@ def get_data():
     return X, y
 
 
+def get_data_prev():
+    data_X = np.array([[]])
+    data_Y = np.array([[]])
+    for config, j in zip(config_files, range(len(config_files))):
+        df = pd.read_csv(config, usecols=features)
+        df_y = pd.read_csv('best_config_file.csv')
+        for i in range(df['Phase'].size):
+            data_X = np.append([data_X], df.iloc[i])
+            data_Y = np.append(data_Y, df_y.iloc[i])
+    data_X = data_X.reshape(8864, 8)
+    X = torch.Tensor(data_X)
+    y = torch.Tensor(data_Y.ravel())
+    return X, y
+
+
 def train_RNN(model, label, input):
     label = label.long()
     print(type(label))
@@ -42,19 +57,22 @@ def train_RNN(model, label, input):
     predictions = []
     losses = []
     criterion = nn.NLLLoss()
-    hidden = model.initHidden()
-    model.zero_grad()
     for i in range(input.size()[0]):
+        hidden = model.initHidden()
+        model.zero_grad()
+
         predicted, hidden = model(input[i].reshape(1, -1), hidden)
         predictions.append(np.argmax(predicted.detach().numpy(), axis=-1))
 
-    loss = criterion(predicted, label[i:i+1])
-    loss.backward()
-    losses.append(loss.item())
+        if i % 4 == 0:
+            loss = criterion(predicted, label[i:i+1])
+            loss.backward(retain_graph=True)
+            losses.append(loss.item())
 
         # Add parameters' gradients to their values, multiplied by learning rate
-    for p in model.parameters():
-        p.data.add_(-learning_rate, p.grad.data)
+            for p in model.parameters():
+                if p.grad is not None:
+                    p.data.add_(-learning_rate, p.grad.data)
 
     return np.vstack(tuple(predictions)), np.mean(np.array(losses))
 
@@ -62,7 +80,7 @@ def train_RNN(model, label, input):
 
 def train(model, label, input):
     label = label.long()
-    batch_size = 1108
+    batch_size = 16
     learning_rate = 0.0001
     predictions = []
     losses = []
@@ -82,30 +100,32 @@ def train(model, label, input):
         for p in model.parameters():
             p.data.add_(-learning_rate, p.grad.data)
 
-    return np.vstack(tuple(predictions)), np.mean(np.array(losses))
+    return np.hstack(tuple(predictions)), np.mean(np.array(losses))
 
 
 def main():
-    input_size, hidden_size, output_size = 64, 128, 8
+    #input_size, hidden_size, output_size = 64, 128, 8
+    input_size, hidden_size, output_size = 8, 16, 8
     model = MLP(input_size, hidden_size, output_size)
-    X, y = get_data()
-    epochs = 500
-    # print(y.size())
-    # for i in range(epochs):
-    #     output_i, loss = train(model, y, X)
-    #     print("epoch {}".format(i))
-    #     print("accuracy = ", np.sum(output_i == y.numpy())/y.size())
-    #     print("loss: {}".format(loss))
-
-
-    model = RNN(input_size, hidden_size, output_size)
+    #X, y = get_data()
+    X, y = get_data_prev()
     epochs = 500
     print(y.size())
     for i in range(epochs):
-        output_i, loss = train_RNN(model, y, X)
+        output_i, loss = train(model, y, X)
         print("epoch {}".format(i))
-        print("accuracy = ", np.sum(output_i == y.numpy()) / y.size())
+        print("accuracy = ", np.sum(output_i == y.numpy())/y.size())
         print("loss: {}".format(loss))
+
+
+    # model = RNN(input_size, hidden_size, output_size)
+    # epochs = 500
+    # print(y.size())
+    # for i in range(epochs):
+    #     output_i, loss = train_RNN(model, y, X)
+    #     print("epoch {}".format(i))
+    #     print("accuracy = ", np.sum(output_i == y.numpy()) / y.size())
+    #     print("loss: {}".format(loss))
 
 
     # for i in range(X.size(0)):
