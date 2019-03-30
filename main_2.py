e8acedfac407c35f134c6d45a213f1f73bca866b
abhishektaur123 <abhishektaur123@gmail.com>
2019-03-30 01:27:20 -0500

    Updated model with issuses in RNN

---
+++ main_2.py
@@ -0,0 +1,125 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+from model import MLP, RNN
+import matplotlib.pyplot as pl
+import torch.nn as nn
+import pandas as pd
+
+lr = 0.1
+seq_length = 20
+data_time_steps = np.linspace(2, 100, seq_length + 1)
+data = (np.sin(data_time_steps) * 5 + 5).astype(int)
+data.resize((seq_length + 1, 1))
+
+features = ['Normalized integer', 'Normalized floating', 'Normalized control', 'Normalized time avg',
+            'Ratio Memory', 'Ratio branches', 'Ratio call', 'Phase']
+
+config_files = ['processed_config_4_40.csv', 'processed_config_4_60.csv', 'processed_config_4_80.csv',
+                'processed_config_4_100.csv', 'processed_config_8_40.csv', 'processed_config_8_60.csv',
+                'processed_config_8_80.csv', 'processed_config_8_100.csv']
+
+
+def get_data():
+    data_X = []
+    data_Y = pd.read_csv('best_config_file.csv').values
+
+    for config, j in zip(config_files, range(len(config_files))):
+        df = pd.read_csv(config, usecols=features).values
+        data_X.append(df)
+    data_X = np.hstack(tuple(data_X))
+    print(data_X.shape)
+    print(data_Y.shape)
+    X = torch.Tensor(data_X)
+    y = torch.Tensor(data_Y.ravel())
+    return X, y
+
+
+def train_RNN(model, label, input):
+    label = label.long()
+    print(type(label))
+    learning_rate = 0.01
+    predictions = []
+    losses = []
+    criterion = nn.NLLLoss()
+    hidden = model.initHidden()
+    model.zero_grad()
+    for i in range(input.size()[0]):
+        predicted, hidden = model(input[i].reshape(1, -1), hidden)
+        predictions.append(np.argmax(predicted.detach().numpy(), axis=-1))
+
+    loss = criterion(predicted, label[i:i+1])
+    loss.backward()
+    losses.append(loss.item())
+
+        # Add parameters' gradients to their values, multiplied by learning rate
+    for p in model.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
+    return np.vstack(tuple(predictions)), np.mean(np.array(losses))
+
+
+
+def train(model, label, input):
+    label = label.long()
+    print(type(label))
+    batch_size = 1108
+    learning_rate = 0.0001
+    predictions = []
+    losses = []
+    for i in range(0, input.size()[0], batch_size):
+
+        model.zero_grad()
+        criterion = nn.NLLLoss()
+
+        predicted = model(input[i:i + batch_size])
+        predictions.append(np.argmax(predicted.detach().numpy(), axis=-1))
+
+        loss = criterion(predicted, label[i:i + batch_size])
+        loss.backward()
+        losses.append(loss.item())
+
+        # Add parameters' gradients to their values, multiplied by learning rate
+        for p in model.parameters():
+            p.data.add_(-learning_rate, p.grad.data)
+
+    return np.vstack(tuple(predictions)), np.mean(np.array(losses))
+
+
+def main():
+    input_size, hidden_size, output_size = 64, 128, 8
+    model = MLP(input_size, hidden_size, output_size)
+    X, y = get_data()
+    epochs = 500
+    print(y.size())
+    for i in range(epochs):
+        output_i, loss = train(model, y, X)
+        print("epoch {}".format(i))
+        print("accuracy = ", np.sum(output_i == y.numpy())/y.size())
+        print("loss: {}".format(loss))
+
+
+    model = RNN(input_size, hidden_size, output_size)
+    epochs = 500
+    print(y.size())
+    for i in range(epochs):
+        output_i, loss = train_RNN(model, y, X)
+        print("epoch {}".format(i))
+        print("accuracy = ", np.sum(output_i == y.numpy()) / y.size())
+        print("loss: {}".format(loss))
+
+
+    # for i in range(X.size(0)):
+    #     input = X[i:i + 1]
+    #     (pred, context_state) = model.forward(input, context_state, model.w1, model.w2)
+    #     context_state = context_state
+    #     predictions.append(pred.data.numpy().ravel()[0])
+    #
+    # pl.scatter(data_time_steps[:-1], X.data.numpy(), s=90, label="Actual")
+    # pl.scatter(data_time_steps[1:], predictions, label="Predicted")
+    # pl.legend()
+    # pl.show()
+
+
+if __name__ == "__main__":
+    main()
--- model.py
+++ model.py
@@ -1,38 +1,40 @@
-import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.init as init
+import torch
 
 
-class RNN_model(nn.Module):
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
 
-    def __init__(self, input_size, hidden_size, output_size, dtype):
-        super(RNN_model, self).__init__()
-        self.dtype = dtype
-        self.output_size = output_size
-        self.input_size = input_size
         self.hidden_size = hidden_size
-        self.w1 = torch.FloatTensor(input_size, hidden_size).type(dtype)
-        init.normal_(self.w1, 0.0, 0.4)
-        self.w1 = Variable(self.w1, requires_grad=True)
-        self.w2 = torch.FloatTensor(hidden_size, output_size).type(dtype)
-        init.normal_(self.w2, 0.0, 0.3)
-        self.w2 = Variable(self.w2, requires_grad=True)
 
-    def forward(self, input, context_state, w1, w2):
-        i = Variable(torch.Tensor([[7, 8, 9]]).type(torch.FloatTensor))
-        j = Variable(torch.Tensor([[0, 0]]).type(torch.FloatTensor))
-        print(i.shape)
-        print(j.shape)
-        xh_dummy = torch.cat((i, j), 1)
-        xh = torch.cat((input, context_state))
-        print(xh.shape)
-        print(w1.shape)
-        xh = xh.t()
-        print(xh.shape)
-        context_state = torch.tanh(xh.mm(w1))
-        print(context_state.shape)
-        print(w2.shape)
-        out = context_state.mm(w2)
-        return out, context_state
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input):
+        hidden = self.i2h(input)
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+        return output
