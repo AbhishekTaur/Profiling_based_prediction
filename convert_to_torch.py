7452058e909153ce4f20729e0f109f5b9be48224
Abhishek Taur <abhishektaur123@palab.cs.tamu.edu>
2019-04-30 17:01:45 -0500

    model.pt

--- convert_to_torch.py
+++ convert_to_torch.py
@@ -5,7 +5,7 @@ model = MLP(7, 16, 8)
 data_X = [[7.78945338e+02,1.10145554e+03, 1.78500882e+02,2.26806641e-01,2.48689490e-01,9.80860579e+00,1.59348961e-01]]
 X = torch.Tensor(data_X)
 
-traced_script_module = torch.jit.trace(model, X[0])
+traced_script_module = torch.jit.trace(model, X)
 
 
 traced_script_module.save("model.pt")
