4bbb656c79955c1d3b3e5f9f1d008ffe3229d85b
Abhishek Taur <abhishektaur123@gpu01.palab.cs.tamu.edu>
2019-05-14 18:12:26 -0500

    Updated run.sh so as to do run profiling too

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for i in {1..8}
 do
-    nohup python process_file_multithreaded.py &
+    nohup python -m cProfile -o profiling_results process_file_multithreaded.py &
 done
 wait
 nohup python main.py
