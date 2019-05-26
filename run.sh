64ab1d8f6e32f6cc6fc65687dc5f3dc8ba033075
Abhishek Taur <abhishektaur123@palab.cs.tamu.edu>
2019-05-26 14:33:51 -0500

    Updated checkpoints and files

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
-for i in {1..8}
-do
-    nohup python -m cProfile -o profiling_results process_file_multithreaded.py &
-done
+nohup python -m cProfile -o profiling_results process_file_multithreaded.py
 wait
 nohup python -m cProfile -o profiling_results_main main.py
 wait
